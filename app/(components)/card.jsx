7e3b54d2fea6a651a0611c9931781a5a75b5669a
Muhammad Rafay Tahir <rafaytahir243@gmail.com>
2024-10-02 17:04:12 +0500

    feat: update styling

--- app/(components)/card.jsx
+++ app/(components)/card.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Card = ({ createdAt, fileName }) => {
   return (
     <div className="border rounded-lg p-4 shadow-md flex items-center justify-center bg-white w-64 h-32 mx-auto">
+      <div className="text-center">
         <p className="text-sm text-gray-900">{createdAt}</p>
-        <p className="text-lg text-gray-700 mt-2">{fileName}</p>
+        <p className="text-lg font-medium text-gray-700 mt-2">{fileName}</p>
+      </div>
     </div>
   );
 };
